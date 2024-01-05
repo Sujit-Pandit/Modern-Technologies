dbaf981496584b94d54ccaeb97466f5612d975d2
Sujit-Pandit <151859644+Sujit-Pandit@users.noreply.github.com>
2024-01-05 14:22:21 +0530

    Add files via upload

---
+++ BookWebApp/src/main/java/com/servlet/EditServlet.java
@@ -0,0 +1,64 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/editurl")
+public class EditServlet extends HttpServlet {
+	private static final String query="update bookdata set BookName=?,BookEdition=?,BookPrice=? where id=?";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//get PrintWriter
+		PrintWriter pw=res.getWriter();
+		//set content type
+		res.setContentType("text/html");
+		//get ID of Book
+		int id=Integer.parseInt(req.getParameter("id"));
+		//get edit data
+		String bookName=req.getParameter("bookName");
+		String bookEdition=req.getParameter("bookEdition");
+		float bookPrice=Float.parseFloat(req.getParameter("bookPrice"));
+		//Load JDBC Driver
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException cnf) {
+			cnf.printStackTrace();
+		}
+		//generate the connection
+		try(Connection con=DriverManager.getConnection("jdbc:mysql:///book","root","riya");
+			PreparedStatement ps=con.prepareStatement(query);){
+			ps.setString(1, bookName);
+			ps.setString(2, bookEdition);
+			ps.setFloat(3, bookPrice);
+			ps.setInt(4, id);
+			int count=ps.executeUpdate();
+			if(count==1) {
+				pw.print("<h2>Record Updated</h2>");
+			}else {
+				pw.print("<h2>Record Not Updated</h2>");
+			}
+		}catch(SQLException se) {
+				se.printStackTrace();
+				pw.println("<h1>"+se.getMessage()+"</h2>");
+			}
+		pw.println("<a href='home.html'>Home</a>");
+		pw.println("<br>");
+		pw.println("<a href='bookList'>Book List</a>");
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req,res);
+		
+	}
+}
